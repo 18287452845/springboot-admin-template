7b68adf5991ca3b7346bfc9dbdf2a822019767c4
ly <1663929970@qq.com>
2023-06-21 16:23:58 +0800

    init

---
+++ src/main/java/com/admin/springbootadmintemplate/SpringbootAdminTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.admin.springbootadmintemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAdminTemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootAdminTemplateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/admin/springbootadmintemplate/SpringbootAdminTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.admin.springbootadmintemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAdminTemplateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
